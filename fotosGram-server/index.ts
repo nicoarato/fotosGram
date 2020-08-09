c770b9998872c84ea63c0cc1b4ec4f7ed46bb160
Nico Arato <nico.arato@gmail.com>
2020-08-09 00:52:31 -0300

    Encriptando la password.

--- fotosGram-server/index.ts
+++ fotosGram-server/index.ts
@@ -1,10 +1,15 @@
 import Server from './classes/server';
 import userRoutes from './routes/usuario';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 
 const server = new Server();
 
+
+// Body parser
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json() );
 
 // Rutas de mi app
 
