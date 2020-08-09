a61bd1c91e8163b601cdda639e0ad14941b94afe
Nico Arato <nico.arato@gmail.com>
2020-08-08 21:43:38 -0300

    Conectando la base de datos.

--- fotosGram-server/index.ts
+++ fotosGram-server/index.ts
@@ -1,5 +1,8 @@
 import Server from './classes/server';
 import userRoutes from './routes/usuario';
+import mongoose from 'mongoose';
+
+
 const server = new Server();
 
 
@@ -8,6 +11,17 @@ const server = new Server();
 server.app.use('/user', userRoutes);
 
 
+// Conectar db
+mongoose.connect('mongodb://localhost:27017/fotosgram', 
+                { useNewUrlParser: true, useCreateIndex: true}, ( err )=> {
+                    if( err ) throw err;
+
+                    console.log('Base de datos ONLINE');
+                });
+
+
+
+// Levantar express
 server.start(() => {
     console.log(`Servidor corriendo en port: ${server.port}`)
 });
